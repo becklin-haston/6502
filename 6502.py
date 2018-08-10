b5373024dd6e7ac0f95bda54bcef79fb373170b8
unknown <bwhaston@gmail.com>
2018-08-10 07:32:12 -0700

    init commit

---
+++ 6502.py
@@ -0,0 +1,106 @@
+from ByteInteger import ByteInteger
+
+class Sixty502:
+
+    def __init__(self):
+
+        self.registers = {
+            "program_counter": 0,
+            "stack_pointer": 0,
+            "accumulator": 0,
+            "index_x": 0,
+            "index_y": 0
+        }
+
+        self.flags = {
+            "carry": False,
+            "zero": False,
+            "interrupt_disable": False,
+            "decimal": False,
+            "break": False,
+            "overflow": False,
+            "negative": False
+        }
+
+        self.memory = [0] * 256
+
+    def __repr__(self):
+
+        register_status = "\nregister status: \n"
+        for key, value in self.registers.items():
+            register_status += f"\t{key}: {value}\n"
+
+        flag_status = "\nflag status: \n"
+        for key, value in self.flags.items():
+            flag_status += f"\t{key}: {value}\n"
+
+        memory_status = "\nmemory status: \n"
+
+        for address, value in enumerate(self.memory):
+            if value != 0:
+                memory_status += f"\t{address}: {value}\n"
+
+        return register_status + flag_status + memory_status
+
+    def __load(self, register, new_value):
+
+
+        byte_int = ByteInteger(new_value)
+        self.registers[register] = byte_int
+        self.flags["negative"] = True if byte_int.value < 0 else False
+        self.flags["zero"] = True if byte_int.value == 0 else False
+
+    def load_accumulator(self, new_value):
+
+        self.__load("accumulator", new_value)
+
+    def load_index_x(self, new_value):
+
+        self.__load("index_x", new_value)
+
+    def load_index_y(self, new_value):
+
+        self.__load("index_y", new_value)
+
+    def __store(self, source, destination):
+
+        try:
+            self.memory[destination] = self.registers[source]
+        except IndexError as index_error:
+            print("Out of memory bound: memory location {}".format(destination))
+
+    def store_accumulator(self, destination):
+
+        self.__store("accumulator", destination)
+
+    def store_index_x(self, destination):
+
+        self.__store("index_x", destination)
+
+    def store_index_y(self, destination):
+
+        self.__store("index_y", destination)
+
+    def __transfer(self, source, destination):
+        self.registers[destination] = self.registers[source]
+
+    def transfer_a_to_x(self):
+        self.__transfer("accumulator", "index_x")
+
+    def transfer_x_to_a(self):
+        self.__transfer("index_x", "accumulator")
+
+    def transfer_a_to_y(self):
+        self.__transfer("accumulator", "index_y")
+
+    def transfer_y_to_a(self):
+        self.__transfer("index_y", "accumulator")
+
+
+
+proc = Sixty502()
+
+a = ByteInteger(255)
+b = ByteInteger(1)
+c = a + b
+print(c)
---
+++ ByteInteger.py
@@ -0,0 +1,60 @@
+class ByteInteger:
+
+    def __init__(self, value, byte_length=8, signed=False):
+        if value > 255:
+            raise ValueError
+        self.signed = signed # TODO implement signed integers
+        self.byte_length = byte_length
+        self.value = value
+        self.bits = [int(digit) for digit in str(bin(value))[2:]]
+        self.hex = hex(value)[2:]
+        if len(self.bits) < self.byte_length:
+            remainder = self.byte_length - len(self.bits)
+            remainder_digits = remainder * [0]
+            new_bits = remainder_digits + self.bits
+            self.bits = new_bits
+
+    def __str__(self):
+
+        return f"bin: {self.bits}, dec: {self.value}, hex: {self.hex}"
+
+    def __add__(self, other):
+
+        new_bits = [0] * 8
+        self.bits.reverse()
+        other.bits.reverse()
+        carry = 0
+
+        for index in range(self.byte_length):
+            self_digit = self.bits[index]
+            other_digit = other.bits[index]
+            current_eval = carry + self_digit + other_digit
+            if current_eval == 0:
+                new_bits[index] = 0
+            elif current_eval == 1:
+                if carry:
+                    carry = 0
+                new_bits[index] = 1
+            elif current_eval == 2:
+                new_bits[index] = 0
+                carry = 1
+            elif current_eval == 3:
+                new_bits[index] = 1
+
+            try:
+                new_bits[index + 1] = carry
+            except IndexError:
+                pass
+
+        new_bits.reverse()
+
+        str_bits = [str(digit) if digit else "0" for digit in new_bits]
+        str_bits = [str(carry)] + str_bits
+        new_int = int("".join(str_bits), 2)
+        self.bits.reverse()
+        other.bits.reverse()
+        result = ByteInteger(new_int)
+        return result
+
+
+
