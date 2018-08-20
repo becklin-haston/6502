0f4b3fa4293854e973ed45691e03cbc929ffda9a
Nick Fusaro <nifusaro@gmail.com>
2018-08-19 17:09:03 -0700

    Implemented multiplication
    Used shift-and-add instead of russian peasent, so 255*1 doesn't overflow.

--- ArithmeticLogic.py
+++ ArithmeticLogic.py
@@ -41,5 +41,68 @@ def add(first, second, bit_pattern=False):
         str_bits = [str(digit) if digit else "0" for digit in new_bits]
         new_int = int("".join(str_bits), 2)
         return new_int
+    
+def times_2(bit_pattern):
+    #a shimmy-shimmy-shimmy-shimmy-shimmy to the left.
+    
+    bit_new = [0]*8
+    for i in range(8):
+        if bit_pattern[i] == 1:
+            if i-1 == -1:
+                raise ValueError("This number is too large to be doubled.")
+            bit_new[i-1] = 1
+    return bit_new
 
+
+def times_2_n(bit_pattern, n):
+    for i in range(n-1):
+        bit_pattern = times_2(bit_pattern)
+    return bit_pattern
+
+
+def over_2(bit_pattern):
+    #a shimmy-shimmy-shimmy-shimmy-shimmy to the right.
+    bit_new = [0]*8
+    for i in range(8):
+        if bit_pattern[i] == 1:
+            if i+1 > 7:
+                pass #Hope you wanted integer division!
+            else:
+                bit_new[i+1] = 1
+    return bit_new
+    
+
+def multiply(first, second, bit_pattern = False):
+    """
+    Had to give up on the russian peasant because you sort of need
+    something that doesn't overflow when you look at it.
+    
+    Uses left-shifts and adds.
+    
+    For each 1 in first, take a copy of second multiplied by the power of 2 
+    represented by the 1, and add it.
+    
+    eg:
+               1 0 0 1
+               1 0 0 0
+         _____________
+               1 0 0 0
+         1 0 0 0 0 0 0
+    
+       = 1 0 0 1 0 0 0
+       
+    
+    """
+    new_bits = [0]*8
+    
+    if not bit_pattern:
+        first = first.bits
+        second = second.bits
+    
+    for i in zip(second, range(8,0, -1)):
+        if i[0] == 1:
+            addend = times_2_n(first, i[1])
+            new_bits = add(new_bits, addend, bit_pattern = True)
+        
+    return new_bits
 
